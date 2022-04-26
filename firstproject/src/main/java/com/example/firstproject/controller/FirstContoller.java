c089bad49bb30cfaf444e50fb44b6f4aed50f7fe
duhui <dh151446@gmail.com>
2022-04-26 16:19:19 +0900

    MVC practice

--- firstproject/src/main/java/com/example/firstproject/controller/FirstContoller.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstContoller.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FirstContoller{
 
     @GetMapping("/hi")
-    public String niceMeet(Model model){
+    public String meet(Model model){
         model.addAttribute("username", "두희");
         return "greetings";         // templates/greetings.mustache
+    }
+
+    @GetMapping("/bye")
+    public String bye(Model model){
+        model.addAttribute("username", "duhui");
+        return "bye";         // templates/bye.mustache
     }
 }
