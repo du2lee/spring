1df7e624da51e4fe806a0b8a8c0e9782e2ce9ce3
duhui <dh151446@gmail.com>
2022-04-27 09:00:02 +0900

    simple dto

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,14 +1,22 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
 
-    @GetMapping("/article/create")
-    public String createArticle() {
+    @GetMapping("/articles/new")
+    public String newArticle() {
         return "articles/create"; 
     }
-    
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println( form.toString());
+        return "";
+    }   
 }
