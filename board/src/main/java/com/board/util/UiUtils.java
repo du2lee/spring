e838a256e881fd7b8fc752890b0df12ea8d458f7
duhui <dh151446@gmail.com>
2022-05-09 15:53:09 +0900

    remove request annotation

--- board/src/main/java/com/board/util/UiUtils.java
+++ board/src/main/java/com/board/util/UiUtils.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.constant.Method;
 
@@ -12,10 +11,8 @@ import com.board.constant.Method;
 public class UiUtils {
 
 	public String showMessageWithRedirect(
-        @RequestParam(value = "message", required = false) String message,
-        @RequestParam(value = "redirectUri", required = false) String redirectUri,
-        @RequestParam(value = "method", required = false) Method method,
-        @RequestParam(value = "params", required = false) Map<String, Object> params, Model model) {
+        String message, String redirectUri, Method method,
+        Map<String, Object> params, Model model) {
 
 		model.addAttribute("message", message);
 		model.addAttribute("redirectUri", redirectUri);
