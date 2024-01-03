a52edab12dc16e068e4b62903f41344a9906ed70
duhui <dh151446@gmail.com>
2024-01-03 20:35:04 +0900

    feat : 정률 할인 구현

---
+++ src/main/java/duhui/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package duhui.core.discount;
+
+import duhui.core.member.Grade;
+import duhui.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP)
+            return price * discountPercent / 100;
+        return 0;
+    }
+}
