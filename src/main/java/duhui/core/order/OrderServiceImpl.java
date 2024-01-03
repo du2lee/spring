d08538520b46a457ea342e2a13797223ad3ea80c
duhui <dh151446@gmail.com>
2024-01-03 20:35:32 +0900

    fix : DIP, OCP 위반 코드 변경

--- src/main/java/duhui/core/order/OrderServiceImpl.java
+++ src/main/java/duhui/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package duhui.core.order;
 
 import duhui.core.discount.DiscountPolicy;
 import duhui.core.discount.FixDiscountPolicy;
+import duhui.core.discount.RateDiscountPolicy;
 import duhui.core.member.Member;
 import duhui.core.member.MemberRepository;
 import duhui.core.member.MemoryMemberRepository;
@@ -9,7 +10,13 @@ import duhui.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+
+
+//    =====================  DIP, OCP 위반 코드  ===============================
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    ========================================================================
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
