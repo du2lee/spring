922ebc51a388e8438b3a4329da8daa78ddbef7ae
duhui <dh151446@gmail.com>
2024-01-03 21:14:38 +0900

    refactor : AppConfig 파일 리팩토링

--- src/main/java/duhui/core/AppConfig.java
+++ src/main/java/duhui/core/AppConfig.java
@@ -1,5 +1,6 @@
 package duhui.core;
 
+import duhui.core.discount.DiscountPolicy;
 import duhui.core.discount.FixDiscountPolicy;
 import duhui.core.discount.RateDiscountPolicy;
 import duhui.core.member.MemberService;
@@ -10,16 +11,28 @@ import duhui.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy fixRepository(){
+        return new FixDiscountPolicy();
+    }
+
+    private DiscountPolicy rateRepository(){
+        return new RateDiscountPolicy();
+    }
+
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService fixOrderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), fixRepository());
     }
 
     public OrderService rateOrderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), rateRepository());
     }
-
 }
