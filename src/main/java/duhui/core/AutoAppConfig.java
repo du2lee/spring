33e07466ece229c399f4b3f129a1b1cff160f064
duhui <dh151446@gmail.com>
2024-01-11 23:42:25 +0900

    test : 컴포넌트 스캔 수동 등록 예시

--- src/main/java/duhui/core/AutoAppConfig.java
+++ src/main/java/duhui/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package duhui.core;
 
+import duhui.core.member.MemberRepository;
+import duhui.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 등록 예시
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
